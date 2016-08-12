4b43794b40aaf30017b451c0f37b2ec8814f28fe
Alex Couture-Beil <alex@mofo.ca>
2016-08-12 13:29:20 -0400

    made go

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dockerlogs
 docker log utility
+
+
+export GOPATH=`pwd`
+go run src/acb/main.go
--- src/acb/main.go
+++ src/acb/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/docker/engine-api/client"
 	"github.com/docker/engine-api/types"
@@ -11,12 +15,22 @@ import (
 
 // curl --unix-socket /var/run/docker.sock 'http:/containers/1a210a4481b7/logs?stderr=1&stdout=1&timestamps=1&follow=1'
 
-type containerLogs struct {
-	ID string
-	ch chan string
+type logLine struct {
+	timestamp string
+	line      string
 }
 
-func main() {
+type containerLogs struct {
+	ID   string
+	line *logLine
+	ch   chan logLine
+}
+
+type logtail struct {
+	containerLogsList []containerLogs
+}
+
+func NewLogTail() *logtail {
 	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
 	cli, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
 	if err != nil {
@@ -32,23 +46,76 @@ func main() {
 	containerLogsList := []containerLogs{}
 
 	for _, c := range containers {
-		ch := make(chan string, 1000)
+		ch := make(chan logLine, 1000)
 		containerLogsList = append(containerLogsList, containerLogs{
-			c.ID,
-			ch,
+			ID:   c.ID,
+			line: nil,
+			ch:   ch,
 		})
 		fmt.Println(c.ID)
 		go tailLogs(c.ID, ch)
 	}
 
-	for _, c := range containerLogsList {
-		x := <-c.ch
-		fmt.Printf("got: %v\n", x)
+	return &logtail{
+		containerLogsList,
+	}
+}
+
+func (s *logtail) getLine() *logLine {
+	for {
+		for i, _ := range s.containerLogsList {
+			c := &s.containerLogsList[i]
+			if c.line == nil {
+				select {
+				case x := <-c.ch:
+					c.line = &x
+				default:
+				}
+			}
+		}
+
+		mini := -1
+		for i, _ := range s.containerLogsList {
+			c := &s.containerLogsList[i]
+			if c.line != nil {
+				if mini == -1 {
+					mini = i
+				} else {
+					if strings.Compare(s.containerLogsList[mini].line.timestamp, c.line.timestamp) < 0 {
+						mini = i
+					}
+				}
+			}
+		}
+
+		var line *logLine
+		if mini >= 0 {
+			c := &s.containerLogsList[mini]
+			line = c.line
+			c.line = nil
+		}
+
+		if line != nil {
+			return line
+		}
+		//time.Sleep(time.Millisecond)
+	}
+}
+
+func main() {
+
+	lt := NewLogTail()
+
+	time.Sleep(5 * time.Millisecond)
+
+	for {
+		line := lt.getLine()
+		fmt.Printf("got: %v\n", line)
 	}
 
 }
 
-func tailLogs(containerID string, ch chan<- string) {
+func tailLogs(containerID string, ch chan<- logLine) {
 	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
 	cli, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
 	if err != nil {
@@ -64,10 +131,25 @@ func tailLogs(containerID string, ch chan<- string) {
 
 	reader := bufio.NewReader(body)
 
-	x, err := reader.ReadString('\n')
-	if err != nil {
-		panic(err)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			fmt.Printf("exiting\n")
+			return
+		} else if err != nil {
+			log.Fatalf("Failed to read container %v log: %v", containerID, err)
+		}
+		if line[len(line)-1] == '\n' {
+			line = line[:len(line)-1]
+		}
+		//TODO figure out whats in the first 8 bytes
+		line = line[8:]
+
+		x := strings.SplitN(line, " ", 2)
+		ch <- logLine{
+			timestamp: x[0],
+			line:      x[1],
+		}
 	}
 
-	ch <- x
 }
