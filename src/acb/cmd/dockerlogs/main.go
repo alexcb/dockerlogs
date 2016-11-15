b7f14072f1cf1ea672990dbc74a93278df6c4375
Alex Couture-Beil <alex@mofo.ca>
2016-11-15 09:28:47 -0800

    allow container to be specified

--- src/acb/cmd/dockerlogs/main.go
+++ src/acb/cmd/dockerlogs/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 	"time"
 
 	"acb"
@@ -11,17 +13,38 @@ import (
 
 func main() {
 
+	containerArg := flag.String("container", "", "container names, separated by commas")
+	flag.Parse()
+	names := strings.Split(*containerArg, ",")
+	if *containerArg == "" {
+		names = []string{}
+	}
+
 	cli := dockerlogs.MustGetDockerCli()
 	lt := dockerlogs.NewLogTail(cli)
 
 	maxContainerNameLength := dockerlogs.GetMaxContainerNameLength(cli)
-	fmt.Printf("=--==- %v\n", maxContainerNameLength)
 
 	// Sleep to make sure all files have been read by the corresponding thread
 	time.Sleep(10 * time.Millisecond)
 
 	for {
 		containerName, line := lt.GetLine()
+
+		// skip container if not listed
+		if len(names) > 0 {
+			found := false
+			for _, xx := range names {
+				if containerName == xx {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
 		timestamp := line.Timestamp.Format("2006-01-02T15:04:05")
 
 		parsedLog := dockerlogs.ParseLog(line.Line)
